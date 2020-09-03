690ca422331b203b22bdc9a1b57eff8a917847e4
ekadar <e.kadar@reach-i4.com>
2020-09-03 16:38:11 +0200

    input, state and state length fields coded as seen in example task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+// implemented functions: 
+// 1. one setstate is implemented
+// 2. character length, negative character length, on the fly changing state, and reversed string of on the fly changing state is displayed
+// 3. in case we type in the 2nd input field, character is reversed first, then original state is set
+// 4. in case we type into the 2nd input field, we can type a character anywhere but then the cursor jumps to the beginning of the input field
+
+
+// helper function to reverse strings
+function reverse(s) {
+  return s.split("").reverse().join("");
+}
 
 function App() {
+
+  const [inputData, setInputData] = useState("");
+
+  function addInputToReversed(e) {
+    setInputData(reverse(e.target.value));
+    e.target.selectionStart = 0;
+    e.target.selectionEnd = 0;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {inputData.length}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{inputData}</h1>
+        <input type="text" value={inputData} onChange={e => setInputData(e.target.value)} />
+        <p>
+          {inputData.length * (-1)}
+        </p>
+        <h1>{reverse(inputData)}</h1>
+        <input type="text" value={reverse(inputData)} onChange={addInputToReversed} />
       </header>
     </div>
   );
