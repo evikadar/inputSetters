d5ef3982f45f12d5fc686b24ff8e70b2a286548d
ekadar <e.kadar@reach-i4.com>
2020-09-03 16:43:13 +0200

    Updates

---
+++ precache-manifest.9a48420396d95efd176480fc57e75f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969e53df3cc91773f2d14bc596c75744",
+    "url": "/inputSetters/index.html"
+  },
+  {
+    "revision": "5a99a22f86da807eb9dd",
+    "url": "/inputSetters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e9f815daca749839641",
+    "url": "/inputSetters/static/js/2.40d2f3af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inputSetters/static/js/2.40d2f3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a99a22f86da807eb9dd",
+    "url": "/inputSetters/static/js/main.c1476f80.chunk.js"
+  },
+  {
+    "revision": "203d60ccc4d5077369bb",
+    "url": "/inputSetters/static/js/runtime-main.4ecd7c63.js"
+  }
+]);
